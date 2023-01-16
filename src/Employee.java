f399f68346c98b7103d7ae306129eedd1f913800
Homework2 <ivliev.k@yandex.ru>
2023-01-16 21:46:03 +0300

    Initial commit

---
+++ src/Employee.java
@@ -0,0 +1,33 @@
+public class Employee {
+    private String nameSurnamePatronymic;
+    private int department;
+    private int salary;
+    public static int counter = 0;
+    private int id;
+
+    public Employee(String nameSurnamePatronymic, int department, int salary) {
+        this.nameSurnamePatronymic = nameSurnamePatronymic;
+        this.department = department;
+        this.salary = salary;
+    }
+
+    public String getNameSurnamePatronymic() {
+        return nameSurnamePatronymic;
+    }
+
+    public int getDepartment() {
+        return department;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setDepartment(int department) {
+        this.department = department;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+}
